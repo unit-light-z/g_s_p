66cacef808338d41985920b1bd2ec53f0b4aa7bd
unit-light-z <80513662+unit-light-z@users.noreply.github.com>
2021-03-15 17:22:26 +0300

    Не стоим на месте, идем дальше
    Копим, накапливаем, превращаем в код, чтобы руками не вбивать, если что...

---
+++ gsp/test_fixture1.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+
+link = "http://selenium1py.pythonanywhere.com/"
+
+
+class TestMainPage1():
+
+    @classmethod
+    def setup_class(self):
+        print("\nstart browser for test suite..")
+        self.browser = webdriver.Chrome(executable_path=r"D:\Projects\selenium_python\chromedriver\chromedriver.exe")
+
+    @classmethod
+    def teardown_class(self):
+        print("quit browser for test suite..")
+        self.browser.quit()
+
+    def test_guest_should_see_login_link(self):
+        self.browser.get(link)
+        self.browser.find_element_by_css_selector("#login_link")
+
+    def test_guest_should_see_basket_link_on_the_main_page(self):
+        self.browser.get(link)
+        self.browser.find_element_by_css_selector(".basket-mini .btn-group > a")
+
+
+class TestMainPage2():
+
+    def setup_method(self):
+        print("\nstart browser for test..")
+        self.browser = webdriver.Chrome(executable_path=r"D:\Projects\selenium_python\chromedriver\chromedriver.exe")
+
+    def teardown_method(self):
+        print("quit browser for test..")
+        self.browser.quit()
+
+    def test_guest_should_see_login_link(self):
+        self.browser.get(link)
+        self.browser.find_element_by_css_selector("#login_link")
+
+    def test_guest_should_see_basket_link_on_the_main_page(self):
+        self.browser.get(link)
+        self.browser.find_element_by_css_selector(".basket-mini .btn-group > a")
